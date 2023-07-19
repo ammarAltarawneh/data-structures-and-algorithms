f51630bc4a1b4fb025e89e79d1f65964b4d78629
ammarAltarawneh <tarawnhammar@gmail.com>
2023-07-19 21:27:18 +0000

    update

---
+++ stack-queue-animal-shelter/README.md
@@ -0,0 +1,16 @@
+## Summary
+Create a class called AnimalShelter which holds only dogs and cats, using a first-in, first-out approach. with these methods:
+
+Enqueue Arguments: animal
+
+Dequeue Arguments: pref pref can be either "dog" or "cat" Return: either a dog or a cat, based on preference.
+
+## Approach & Efficiency
+Enqueue method time complixity is O(1) Dequeue method time complixity is O(1)
+
+## Solution
+To Enqueue or Dequeue you should create an AnimalShelter object, a Cat or Dog object and call the method.
+
+animalShelter.Enqueue(Cat);
+
+animalShelter.Dequeue("cat");
---
+++ stack-queue-animal-shelter/stack-queue-animal-shelter/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace stack_queue_animal_shelter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Queue queue = new Queue();
+            //queue.Enqueue("1");
+            //queue.Enqueue("2");
+            //queue.Enqueue("3");
+            //Console.WriteLine(queue.ToString());
+            //queue.Dequeue();
+            //Console.WriteLine(queue.ToString());
+
+            //PseudoQueue pseudoQueue = new PseudoQueue();
+            //pseudoQueue.Enqueue("1");
+            //pseudoQueue.Enqueue("2");
+            //pseudoQueue.Enqueue("3");
+            //pseudoQueue.Enqueue("4");
+            //pseudoQueue.Dequeue();
+
+            AnimalShelter animalShelter = new AnimalShelter();
+            Animal cat = new Cat();
+            animalShelter.Enqueue(new Dog());
+            animalShelter.Enqueue(new Dog());
+            animalShelter.Enqueue(cat);
+            animalShelter.Enqueue(cat);
+            animalShelter.Enqueue(cat);
+            Console.WriteLine(animalShelter.Dequeue("cat"));
+            Console.WriteLine(animalShelter.Dequeue("cat"));
+            Console.WriteLine(animalShelter.Dequeue("dog"));
+            Console.WriteLine(animalShelter.Dequeue("at"));
+        }
+    }
+}
